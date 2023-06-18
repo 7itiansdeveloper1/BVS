5db9ab1d76eb7896e1d42cc4fb0423ff01f92d65
itvisionrampur <itvisionrampur@gmail.com>
2023-06-18 07:48:54 +0530

    Verions D1

--- iSAS/obj/Release/Package/PackageTmp/dist/custom js/DailyAttendanceLanding.js
+++
@@ -1,5 +0,0 @@
-﻿/// <reference path="../angular.js" />
-var dailyAttendance = angular.module("dailyAttendance", []);
-dailyAttendance.controller("dailyAttendanceController", function ($scope, $http) {
-
-});
